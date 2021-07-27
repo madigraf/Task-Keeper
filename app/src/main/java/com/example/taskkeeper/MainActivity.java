60a41d94d6d5a99d52a39bd8114e5de1b358ae7e
madigraf <grafmadison@gmail.com>
2021-07-27 12:36:28 -0700

    Added bottom navigation bar

--- app/src/main/java/com/example/taskkeeper/MainActivity.java
+++ app/src/main/java/com/example/taskkeeper/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.taskkeeper;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize Bottom Navigation View
+        BottomNavigationView navView = findViewById(R.id.bottomNav_view);
+
+        // Pass the ID's of Different destinations
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_today, R.id.navigation_week, R.id.navigation_maintasks,
+                R.id.navigation_archivetasks, R.id.navigation_routines )
+                .build();
+
+        // Initialize NavController
+        NavController navController = Navigation.findNavController(this, R.id.navHostFragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
     }
 }
